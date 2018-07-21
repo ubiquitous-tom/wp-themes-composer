4984dac49301bb1ddc8befdd6b370f843bf67eb5
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-07-20 18:31:56 -0700

    Added update password subsection to account management.

---
+++ plugins/rlje-wp-plugin/rlje-account-page/partials/edit-password.php
@@ -0,0 +1,21 @@
+<h3>Change Your Password</h3>
+<h4>If you wish to change your password, Please provide the details below.</h4>
+<p>
+	<small>Please enter a minimum of 6 characters.</small>
+</p>
+<?php if ( ! empty( $message_error ) ) { ?>
+	<p class="bg-danger"><?php echo $message_error; ?></p>
+<?php } elseif ( ! empty( $message_sucess ) ) { ?>
+	<p class="bg-success"><?php echo $message_sucess; ?></p>
+<?php } ?>
+<form id="user-change-password" method="POST">
+	<div class="form-group">
+		<label for="new-password">New Password</label>
+		<input id="new-password" class="form-control" name="new-password" type="password" required>
+	</div>
+	<div class="form-group">
+		<label for="new-password-confirm">Confirm New Password</label>
+		<input id="new-password-confirm" class="form-control" name="new-password-confirm" type="password" required>
+	</div>
+	<button type="submit" class="btn btn-primary">Change Password</button>
+</form>
