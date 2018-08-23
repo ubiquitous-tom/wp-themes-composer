3bb669941202f69e2c2e1b9cc59378df47f4c7e4
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-08-23 15:13:27 -0700

    Made the top nav bar apear as collapsed on small devices as well

--- plugins/rlje-wp-plugin/rlje-theme-settings/navigations/partials/not-logged-in.php
+++ plugins/rlje-wp-plugin/rlje-theme-settings/navigations/partials/not-logged-in.php
@@ -3,7 +3,7 @@
 		<a class="sign-in" href="<?php echo esc_url( trailingslashit( home_url( 'signin' ) ) ); ?>"><?php echo esc_html( $navigation_text['login'] ); ?></a>
 	</li>
 	<li class="pull-right" >
-		<a class="sign-up hidden-md hidden-lg" href="<?php echo esc_url( trailingslashit( home_url( 'signup' ) ) ); ?>"><?php echo esc_html( $navigation_text['signup'] ); ?></a>
-		<a class="sign-up btn btn-primary visible-md-block visible-lg-block" href="<?php echo esc_url( trailingslashit( home_url( 'signup' ) ) ); ?>"><?php echo esc_html( $navigation_text['free_trial'] ); ?></a>
+		<a class="sign-up hidden-lg" href="<?php echo esc_url( trailingslashit( home_url( 'signup' ) ) ); ?>"><?php echo esc_html( $navigation_text['signup'] ); ?></a>
+		<a class="sign-up btn btn-primary visible-lg-block" href="<?php echo esc_url( trailingslashit( home_url( 'signup' ) ) ); ?>"><?php echo esc_html( $navigation_text['free_trial'] ); ?></a>
 	</li>
 </ul>
