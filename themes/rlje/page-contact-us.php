cb79624160f0c0336dca92fa8518bd6ca61478b4
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-08-29 15:13:59 -0700

    Updated all page templates to follow the same markup

--- themes/rlje/page-contact-us.php
+++ themes/rlje/page-contact-us.php
@@ -2,53 +2,50 @@
 get_header();
 ?>
 
-<section id="contact-hero">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h3>Contact Us</h3>
+<section class="page-main">
+	<header class="page-hero">
+		<div class="container">
+			<h3>Contact Us</h3>
+		</div>
+	</header>
+	<section class="page-body" id="contact-form">
+		<div class="container">
+			<div class="col-sm-8 col-sm-offset-2 well well-lg">
+
+				<div id="msg"></div>
+
+				<form method="post" id="contact-us">
+					<p>
+						If you’re having trouble with watching films, signing up, or any difficulty with using the site, please see our <a href="https://support.umc.tv">support page</a> for assistance. Many common solutions are provided on our <a href="http://support.acorn.tv/support/solutions/folders/1000220893"> Frequently Asked Questions</a> page. For all other inquiries, please use the form below and we’ll get in touch.
+					</p>
+
+					<div class="form-group">
+						<label for="full-name">Name</label>
+						<input type="text" id="full-name" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="email">E-Mail Address</label>
+						<input type="email" id="email" name="email" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="subject">Subject</label>
+						<input type="text" id="subject" name="subject" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="description">Message</label>
+						<textarea id="description" title="" name="description" rows="6" class="form-control" required></textarea>
+					</div>
+
+					<button class="btn btn-primary btn-large btn-block">Submit</button>
+
+				</form>
 			</div>
-		</div>
-	</div>
-</section>
-<section id="contact-form">
-	<div class="container browse">
-		<div id="contactus-view" class="url-view">
-
-			<div id="msg"></div>
-
-			<form method="post" id="contact-us">
-
-				<p>
-					If you’re having trouble with watching films, signing up, or any difficulty with using the site, please see our <a href="https://support.umc.tv">support page</a> for assistance. Many common solutions are provided on our <a href="http://support.acorn.tv/support/solutions/folders/1000220893"> Frequently Asked Questions</a> page. For all other inquiries, please use the form below and we’ll get in touch.
-				</p>
-
-				<div class="form-group">
-					<label for="full-name">Name</label>
-					<input type="text" id="full-name" class="form-control" required>
-				</div>
-
-				<div class="form-group">
-					<label for="email">E-Mail Address</label>
-					<input type="email" id="email" name="email" class="form-control" required>
-				</div>
-
-				<div class="form-group">
-					<label for="subject">Subject</label>
-					<input type="text" id="subject" name="subject" class="form-control" required>
-				</div>
-
-				<div class="form-group">
-					<label for="description">Message</label>
-					<textarea id="description" title="" name="description" rows="6" class="form-control" required></textarea>
-				</div>
-
-				<button class="btn btn-primary btn-large btn-block">Submit</button>
-
-			</form>
 
 		</div>
-	</div>
+	</section>
 </section>
 
 <?php
