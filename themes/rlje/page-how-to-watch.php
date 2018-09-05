febdf0058994a469106ffb8882b5fa29331a1a4e
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-09-04 18:45:56 -0700

    Further updates to make how to watch page look better on mobile

--- themes/rlje/page-how-to-watch.php
+++ themes/rlje/page-how-to-watch.php
@@ -60,10 +60,10 @@ get_header();
 	<section class="choices tab-content container">
 		<div id="watch-tv" class="tab-pane active">
 			<div class="appletv row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/apple_tv.png'; ?>" alt="Apple TV logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Apple TV</h5>
 					<p>Visit the App Store to download <?php echo get_bloginfo( 'name' ); ?> to your new Apple TV.
 					<br><small>Watching on a legacy Apple TV? Use AirPlay to stream to your TV.</small></p>
@@ -73,10 +73,10 @@ get_header();
 				</div>
 			</div>
 			<div class="roku row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/roku_new.png'; ?>" alt="Roku logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Roku</h5>
 					<p>Visit Roku's channel store and search for <strong><?php echo get_bloginfo( 'name' ); ?></strong> to add the channel.</p>
 					<p class="device-link">
@@ -85,10 +85,10 @@ get_header();
 				</div>
 			</div>
 			<div class="fire-tv row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/amazon_logo.png'; ?>" alt="Availabe on Amazon logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Amazon Fire TV</h5>
 					<p>Visit Amazon's App store to download <?php echo get_bloginfo( 'name' ); ?> to your Amazon Fire Device</p>
 					<p class="device-link">
@@ -97,10 +97,10 @@ get_header();
 				</div>
 			</div>
 			<div class="chromecast row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/chromecast_logo.png'; ?>" alt="Chromecast logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Chromecast</h5>
 					<p>To stream via Chromecast, simply click the Chromecast icon located in the top right corner of the iOS and Android apps.</p>
 					<p class="device-link">
@@ -111,10 +111,10 @@ get_header();
 		</div>
 		<div id="watch-mobile" class="tab-pane">
 			<div class="ios row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/app_store_badge.png'; ?>" alt="App store badge">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>iPhone &amp; iPad App</h5>
 					<p>Visit the App Store to download <?php echo get_bloginfo( 'name' ); ?> on your iPad, iPhone or iPod Touch.</p>
 					<p class="device-link">
@@ -123,10 +123,10 @@ get_header();
 				</div>
 			</div>
 			<div class="android row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/googleplay_logo.png'; ?>" alt="Google Play Store logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Android App</h5>
 					<p>Visit the Google Play Store to download <?php echo get_bloginfo( 'name' ); ?> on your Android devices.</p>
 					<p class="device-link">
@@ -137,10 +137,10 @@ get_header();
 		</div>
 		<div id="watch-web" class="tab-pane">
 			<div class="online row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/browsers.png'; ?>" alt="Browsers logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Watch Online</h5>
 					<p>Watch <?php echo get_bloginfo( 'name' ); ?> on your computer or tablet via web browser</p>
 					<p class="device-link">
@@ -151,10 +151,10 @@ get_header();
 		</div>
 		<div id="watch-amazon" class="tab-pane">
 			<div class="amazon-channels row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/amazon_prime_channels.png'; ?>" alt="Prime Channels Logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Available on Amazon Channels</h5>
 					<p>Prime members can subscribe to <?php echo get_bloginfo( 'name' ); ?> via Amazon</p>
 					<p class="device-link">
@@ -165,10 +165,10 @@ get_header();
 		</div>
 		<div id="watch-xfinity" class="tab-pane">
 			<div class="xfinity row">
-				<div class="col-sm-5 col-md-3">
+				<div class="col-xs-5 col-sm-5 col-md-3">
 					<img class="device-logo center-block" src="<?php echo get_template_directory_uri() . '/img/xfinity_logo_black.jpg'; ?>" alt="Xfinity logo">
 				</div>
-				<div class="col-sm-7 col-md-9">
+				<div class="col-xs-7 col-sm-7 col-md-9">
 					<h5>Xfinity</h5>
 					<p><?php echo get_bloginfo( 'name' ); ?> is available as an add-on channel to Xfinity TV customers.</p>
 					<p class="device-link">
