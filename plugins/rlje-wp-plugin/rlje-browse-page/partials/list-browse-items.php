8847339423377288d0a3902e3815c1ccb8e6e37b
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-09-18 17:47:32 -0700

    Cleaned up amd fixed browse page individual items template

--- plugins/rlje-wp-plugin/rlje-browse-page/partials/list-browse-items.php
+++ plugins/rlje-wp-plugin/rlje-browse-page/partials/list-browse-items.php
@@ -3,9 +3,9 @@
 $list_items       = get_query_var( 'array_list_items' ); //$wp_query->query_vars['array_list_items'];
 $base_url_path    = ( function_exists( 'rljeApiWP_getBaseUrlPath' ) ) ? rljeApiWP_getBaseUrlPath() : '';
 $total_list_items = count( $list_items );
-if ( $total_list_items > 0 ) :
+if ( $total_list_items > 0 ) {
 	$item_count = 0;
-	foreach ( $list_items as $key => $item ) :
+	foreach ( $list_items as $key => $item ) {
 		if ( ! isset( $item->id ) ) {
 			$item->id = $item->franchiseID;
 		}
@@ -22,25 +22,25 @@ if ( $total_list_items > 0 ) :
 		$is_new_row = ( $key % 4 == 0 ) ? true : false;
 		$item_count++;
 		$data_a_z = preg_replace( '/^The\s(.+)/i', '$1', strtolower( $item->name ) );
-		if ( $is_new_row ) :
+		if ( $is_new_row ) {
 			$item_count = 0;
 			?>
-<div class="row">
-<?php endif; ?>
-	<div class="col-sm-6 col-md-6 col-lg-3" itemscope itemtype="http://schema.org/TVSeries" data-az="<?php echo esc_attr( $data_a_z ); ?>" data-added="<?php echo esc_attr( $key + 1 ); ?>">
-		<a itemprop="url" href="<?php echo esc_url( trailingslashit( home_url( $item->id ) ) ); ?>">
-			<?php $item_image = apply_filters( 'rlje_franchise_artwork', $item->image, $item ); ?>
-			<img title="<?php echo esc_attr( $item->name ); ?>" alt="<?php echo esc_attr( $item->id ); ?>" class="wp-post-image" itemprop="image" src="<?php echo esc_url( rljeApiWP_getImageUrlFromServices( $item_image . '?t=titled-avatars&w=550' ) ); ?>" style="width:100%; height:auto;" />
-		</a>
-		<p itemprop="name" class="franchise-title"><?php echo esc_html( $item->name ); ?></p>
-	</div>
-<?php if ( $item_count === 3 || ( $key === $total_list_items - 1 ) ) : ?>
-</div>
-<?php
-endif;
-	endforeach;
-else :
-?>
+			<div class="row">
+		<?php } ?>
+		<div class="col-sm-6 col-md-6 col-lg-3" itemscope itemtype="http://schema.org/TVSeries" data-az="<?php echo esc_attr( $data_a_z ); ?>" data-added="<?php echo esc_attr( $key + 1 ); ?>">
+			<a itemprop="url" href="<?php echo esc_url( trailingslashit( home_url( $item->id ) ) ); ?>">
+				<?php $item_image = apply_filters( 'rlje_franchise_artwork', $item->image, $item ); ?>
+				<img title="<?php echo esc_attr( $item->name ); ?>" alt="<?php echo esc_attr( $item->id ); ?>" class="wp-post-image" itemprop="image" src="<?php echo esc_url( rljeApiWP_getImageUrlFromServices( $item_image . '?t=titled-avatars&w=550' ) ); ?>" style="width:100%; height:auto;" />
+			</a>
+			<p itemprop="name" class="franchise-title"><?php echo esc_html( $item->name ); ?></p>
+		</div>
+		<?php if ( $item_count === 3 || ( $key === $total_list_items - 1 ) ) { ?>
+			</div>
+		<?php
+		}
+	}
+} else {
+	?>
 <div class="row">
 	<?php
 	// if ( $wp_query->query_vars['section'] === 'yourwatchlist' ) {
@@ -52,11 +52,5 @@ else :
 	set_query_var( 'no_result_inline', true );
 	get_template_part( 'partials/no-result-message' );
 	?>
-	<script>
-		document.addEventListener("DOMContentLoaded", function() {
-			jQuery('li.item').css('cursor','default');
-		});
-	</script>
 </div>
-	<?php
-	endif;
+<?php }
