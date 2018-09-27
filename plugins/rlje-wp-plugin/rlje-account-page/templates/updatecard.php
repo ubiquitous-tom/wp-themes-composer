2b629b6e79ac283378296a1ea7fc6089bb56d7fe
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-09-26 18:33:10 -0700

    Made stunning ID configurable on WP backend

--- plugins/rlje-wp-plugin/rlje-account-page/templates/updatecard.php
+++ plugins/rlje-wp-plugin/rlje-account-page/templates/updatecard.php
@@ -1,9 +1,7 @@
 <?php
 get_header();
-$stripe_id = $this->user_profile['Customer']['StripeCustomerID'];
-$stunning_token = '2047dxrbmvipxdnrcfbetfxoe';
-$stunning_url = 'https://payments.stunning.co/payment_update/' . $stunning_token . '/' . $stripe_id;
 ?>
+
 <iframe id="stunningframe" sandbox="allow-forms allow-scripts allow-same-origin" src="<?php echo $stunning_url; ?>">
 </iframe>
 
