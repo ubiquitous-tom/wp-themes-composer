c78974f5c51ceaf6867886b76947ad60fd373d80
Sasan Namiranian <snamiranian@rljentertainment.com>
2018-10-01 12:31:39 -0700

    Disable and show a spinning wheel on contact-us page form submit button

--- themes/rlje/js/contactForm.js
+++ themes/rlje/js/contactForm.js
@@ -1,6 +1,11 @@
 jQuery(function($) {
 
     $('form#contact-us').on('submit', function(event) {
+        var submit_button = jQuery(this).find('button');
+        var submit_button_width = submit_button.width();
+        var submit_button_content = submit_button.html();
+        submit_button.prop('disabled', true).html('<i class="fa fa-circle-o-notch fa-spin"></i>');
+        submit_button.width(submit_button_width);
         event.preventDefault();
         var name = $(this).find('input#full-name').val();
         var email_address = $(this).find('input#email').val();
@@ -22,6 +27,7 @@ jQuery(function($) {
                     $('#msg').html("<div class=\"alert alert-success\"><strong>Thank you for contacting UMC.</strong><br>We’ve received your email. A UMC support representative will review your request and send you a personal response.</div>");
                 }
                 $(window).scrollTop(0);
+                submit_button.prop('disabled', false).html(submit_button_content);
             }
         )
     })
